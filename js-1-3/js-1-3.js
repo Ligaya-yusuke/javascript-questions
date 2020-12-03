e33341440dbd4c9d18985e0c9854d45fdd0e2fb0
Ligaya-yusuke <e98069@gmail.com>
2020-12-03 18:56:29 +0900

    課題①ー3修正

--- js-1-3/js-1-3.js
+++ js-1-3/js-1-3.js
@@ -1,18 +1,18 @@
 console.log('===========================')
 console.log('現在持っているのタスク一覧')
 console.log('===========================')
-let task = ['掃除', '買い物', '散歩'];
-for(i=0; i < task.length; i++) {
-  console.log(`${i} : ` + task[i]);
-};
+const tasks = ['掃除', '買い物', '散歩'];
+tasks.forEach(function(value, index){
+  console.log(`${index} : `, value);
+});
 
 const answer = window.prompt('タスクを入力してください');
-task.push(answer);
+tasks.push(answer);
 alert('新しいタスクを追加しました。');
 console.log('===========================')
 console.log('現在持っているのタスク一覧')
 console.log('===========================')
-for(i=0; i < task.length; i++) {
-  console.log(`${i} : ` + task[i]);
-}
+tasks.forEach(function(value, index){
+  console.log(`${index} : `, value);
+});
 window.prompt('「確認,追加,削除,終了」の4つのいずれかを入力してください');
