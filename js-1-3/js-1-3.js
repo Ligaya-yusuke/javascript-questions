be09cf0400c5a499d7b4fed4cc0245552e8e7a36
Ligaya-yusuke <e98069@gmail.com>
2020-12-04 11:15:05 +0900

    課題①ー3修正2送付

--- js-1-3/js-1-3.js
+++ js-1-3/js-1-3.js
@@ -6,13 +6,17 @@ tasks.forEach(function(value, index){
   console.log(`${index} : `, value);
 });
 
-const answer = window.prompt('タスクを入力してください');
-tasks.push(answer);
-alert('新しいタスクを追加しました。');
-console.log('===========================')
-console.log('現在持っているのタスク一覧')
-console.log('===========================')
-tasks.forEach(function(value, index){
-  console.log(`${index} : `, value);
-});
+function answer() {
+  const task = window.prompt('タスクを入力してください');
+  tasks.push(task);
+  alert('新しいタスクを追加しました。');
+  console.log('===========================')
+  console.log('現在持っているのタスク一覧')
+  console.log('===========================')
+  tasks.forEach(function(value, index){
+    console.log(`${index} : `, value);
+  });
+}
+answer();
+
 window.prompt('「確認,追加,削除,終了」の4つのいずれかを入力してください');
